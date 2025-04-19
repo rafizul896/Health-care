3093d0f16329f4a46b7d87d4b9e26eacf8e8162a
rafizul896 <rafizulislam896@gmail.com>
2025-04-19 15:15:05 +0600

    add: Search/Filter Functionality Using searchTerm Query

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,13 +2,23 @@ import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllFromDB();
+  try {
+    const result = await AdminService.getAllFromDB(req.query);
 
-  res.status(200).json({
-    success: true,
-    message: "Admin data fetched.",
-    data: result,
-  });
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched.",
+      data: result,
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: err?.name || "Something went wrong",
+        error: err
+      });
+    }
+  }
 };
 
 export const AdminController = {
