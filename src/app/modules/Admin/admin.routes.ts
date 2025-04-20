5ebc9aabc6000eb0329aef2e1f4c7d2b01bd368d
rafizul896 <rafizulislam896@gmail.com>
2025-04-21 05:44:44 +0600

    add: request validation middleware

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { AdminController } from "./admin.controller";
-
+import validateRequest from "../../utils/validateRequest";
+import { AdminValidation } from "./admin.validation";
 const router = Router();
 
 router.get("/", AdminController.getAllFromDB);
+
 router.get("/:id", AdminController.getByIdFromDB);
-router.patch("/:id", AdminController.updateIntoDB);
+
+router.patch(
+  "/:id",
+  validateRequest(AdminValidation.updateValidationSchema),
+  AdminController.updateIntoDB
+);
+
 router.delete("/:id", AdminController.deleteFromDB);
+
 router.delete("/soft/:id", AdminController.softDeleteFromDB);
 
 export const AdminRoutes = router;
